cd0549d6112309319f6cffa432b5c324fe2b6bd2
liuxiaoyu <610066482@qq.com>
2018-07-19 13:39:10 +0800

    测试保存

--- JavaTest/src/file/FileTest.java
+++ JavaTest/src/file/FileTest.java
@@ -1,9 +1,14 @@
 package file;
 
+
 import java.io.File;
+import java.util.Scanner;
 
 public class FileTest {
-	public static void showFoulderSize(String path) {
+	
+	private static int printLength = 0;
+	
+	private static void showFoulderSize(String path) {
 		File f = new File(path);
 		//System.out.println(new File("C:\\Users\\All Users\\Dell\\SARemediation").canWrite());
 		File files[] = f.listFiles();
@@ -11,11 +16,11 @@ public class FileTest {
 		long length = 0;
 		for(File file:files) {
 			if(file.isDirectory()) {
-				System.out.print("目录："+file.getName()+": ");
+				System.out.print("drct: "+file.getName()+": ");
 				length = getFoulderSize(file);
 			}
 			else if(file.isFile()) {
-				System.out.print("文件："+file.getName()+": ");
+				System.out.print("file: "+file.getName()+": ");
 				length = file.length();
 			}
 			//System.err.println(file.getName());
@@ -23,32 +28,44 @@ public class FileTest {
 			total += length;
 			length = 0;
 		}
-		System.err.print("总计大小："+formatSize(total));
+		System.err.print("total size: "+formatSize(total));
 	}
 	
-	public static void showFoulderSizes(String path) {
+	private static void showFoulderSizes(String path) {
+		deletePrint(printLength);
+		String str = "now caculating size of foulder "+path;
+		printLength = str.length();
+		System.out.print(str);
 		File f = new File(path);
 		File files[] = f.listFiles();
 		long total = 0;
 		long length = 0;
 		for(File file:files) {
 			if(file.isDirectory()) {
-				System.out.print("目录："+file.getName()+": ");
 				length = getFoulderSize(file);
+				deletePrint(printLength);
+				System.out.println("drct: "+file.getName()+": "+formatSize(length));
 			}
 			else if(file.isFile()) {
-				System.out.print("文件："+file.getName()+": ");
 				length = file.length();
+				System.out.println("file: "+file.getName()+": "+formatSize(length));
 			}
 			//System.err.println(file.getName());
-			System.err.println(formatSize(length));
 			total += length;
 			length = 0;
 		}
-		System.err.print("总计大小："+formatSize(total));
+		System.err.print("total size: "+formatSize(total));
 	}
 	
-	public static long getFoulderSize(File file) {
+	private static void deletePrint(int length) {
+		String str="";
+		while(length>0) {
+			str+="\b";
+		}
+		//System.out.print(str);
+	}
+	
+	private static long getFoulderSize(File file) {
 		long length = 0;
 		File files[] = file.listFiles();
 		if(files == null) return 0;
@@ -60,16 +77,19 @@ public class FileTest {
 		return length;
 	}
 	
-	public static String formatSize(long size) {
+	private static String formatSize(long size) {
 		if(size > 1024*1024*1024) return String.format("%.2f", (double)size/1024/1024/1024)+" GB";
 		else if(size > 1024*1024) return String.format("%.2f", (double)size/1024/1024)+" MB";
 		else if(size > 1024) return String.format("%.2f", (double)size/1024)+" KB";
 		else return size + " B";
 	}
 	
-	public static void main2(String[] args) {
+	public static void main(String[] args) {
 		//showFoulderSize("C:\\Users");
-		showFoulderSizes("F:\\工作文件\\研发组");
+		Scanner s = new Scanner(System.in);
+		String path = s.next();
+		s.close();
+		showFoulderSizes(path);
 		//System.out.println(formatSize(1000000000000l));
 		//System.out.println(formatSize(new File("F:/ubuntu-16.04.3-desktop-amd64.iso").length()));
 	}
