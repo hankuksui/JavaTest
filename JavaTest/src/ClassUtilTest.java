d73c4fe167e14f9bdca0b78013efc669316ec966
liuxiaoyu <610066482@qq.com>
2018-07-20 21:03:39 +0800

    java执行java

--- JavaTest/src/ClassUtilTest.java
+++ JavaTest/src/ClassUtilTest.java
@@ -5,23 +5,29 @@ import java.util.Map;
 public class ClassUtilTest
 {
 	public static void main(String args[]){
-		
-		ClassUtil.CLASS_PATH = ClassUtilTest.class.getResource("").toString();
+		String path = ClassUtilTest.class.getResource("").toString();
+		ClassUtil.CLASS_PATH = path.substring(path.indexOf(":")+2);
 		
 		StringBuilder sb = new StringBuilder();
 		//sb.append("package com.even.test;");
+		if(ClassUtilTest.class.getPackage() != null) sb.append("package "+ClassUtilTest.class.getPackage().getName()+";");
 		sb.append("import java.util.Map;\nimport java.text.DecimalFormat;\n");
 		sb.append("public class Sum{\n");
-		sb.append("private final DecimalFormat df = new DecimalFormat(\"#.#####\");\n");
-		sb.append("public Double calculate(Map<String,Double> data){\n");
-		sb.append("double d = (30*data.get(\"f1\") + 20*data.get(\"f2\") + 50*data.get(\"f3\"))/100;\n");
-		sb.append("return Double.valueOf(df.format(d));}}\n");
+		sb.append("	private final DecimalFormat df = new DecimalFormat(\"#.#####\");\n");
+		sb.append("	public Double calculate(Map<String,Double> data){\n");
+		sb.append("		double d = (30*data.get(\"f1\") + 20*data.get(\"f2\") + 50*data.get(\"f3\"))/100;\n");
+		sb.append("		return Double.valueOf(df.format(d));\n");
+		sb.append("	}\n");
+		sb.append("	public String getHelloStr(String name){\n");
+		sb.append("		return \"Hello, \"+name+\"!\";\n");
+		sb.append("	}\n");
+		sb.append("}\n");
 		//设置编译参数
 		ArrayList<String> ops = new ArrayList<String>();
 		ops.add("-Xlint:unchecked");
 		//编译代码，返回class
 //		Class<?> cls = ClassUtil.loadClass("/com/even/test/Sum.java",sb.toString(),"com.even.test.Sum",ops);
-		Class<?> cls = ClassUtil.loadClass("/Sum.java",sb.toString(),"Sum",ops);
+		Class<?> cls = ClassUtil.loadClass("Sum.java",sb.toString(),"Sum",ops);
 		//准备测试数据
 		Map<String,Double> data = new HashMap<String,Double>();
 		data.put("f1", 10.0);
@@ -32,5 +38,6 @@ public class ClassUtilTest
 		//输出结果
 		System.err.println("data: "+data);
 		System.out.println("(30*f1+20*f2+50*f3)/100 = "+result);
+		System.out.println("I am lxy: "+ClassUtil.invoke(cls, "getHelloStr", new Class[] {String.class}, new Object[] {"lxy"}));
 	}
 }
--- JavaTest/src/JavaTest.java
+++ JavaTest/src/JavaTest.java
@@ -1,8 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println(JavaTest.class.getResource(""));
+
+		System.out.println(JavaTest.class.getPackage());
+		
+		String path = JavaTest.class.getResource("").toString();
+		System.out.println(path.substring(path.indexOf(":")+2));
+		
+		JavaTest jt = new JavaTest();
+		Map<String,Double> data = new HashMap<String,Double>();
+		data.put("f1", 10.0);
+		data.put("f2", 20.0);
+		data.put("f3", 30.0);
+		System.out.println(jt.calculate(data));
+	}
+
+	private final DecimalFormat df = new DecimalFormat("#.#####");
+
+	public Double calculate(Map<String, Double> data)
+	{
+		double d = (30 * data.get("f1") + 20 * data.get("f2")
+				+ 50 * data.get("f3")) / 100;
+		return Double.valueOf(df.format(d));
 	}
 }
